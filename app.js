f9c1600c685135c8002a81b2bfa468700a4fa16a
Colin Wong <cwong@securecodewarrior.com>
2020-11-26 14:57:32 +1100

    Added feature

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.get('/foo/:id', (req, res) => {
     eval('console.log("something else", ' + req.params.id + ')');
     // #3
     eval(`console.log("something else", ${req.params.id})`);
+    // #4
+    eval(`console.log("and another one ${req.params.id}")`);
+    // #5
+    eval(`console.log("yet another one ${req.params.id}")`);
+    // #6
+    eval(`console.log("blah ${req.params.id}")`);
+    res.send(`Hello ${req.params.id} and ${req.params.name}`);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
