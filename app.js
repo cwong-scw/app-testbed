774ff1750abdaeae6a82ecec3d5762fe786322e0
cwong-scw <cwong@securecodewarrior.com>
2022-08-17 14:41:32 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/foo/:id', (req, res) => {
+app.get('/foo/:id/:bar', (req, res) => {
     // #1 serious
     eval('console.log("something", ' + req.params.id + ')');
     // #2 super serious
@@ -12,6 +12,7 @@ app.get('/foo/:id', (req, res) => {
     eval(`console.log("something else", ${req.params.id})`);
     
     eval(`console.log("Request with ID ", ${req.params.id})`);
+    eval(req.params.bar);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
